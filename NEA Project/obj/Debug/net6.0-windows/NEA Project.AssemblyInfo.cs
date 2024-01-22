8261f713ca3b5005df4602a649036bd3357dd554
Charlotte Billington <charlotte2404200@gmail.com>
2024-01-22 16:12:30 +0000

    fixed coins

--- NEA Project/obj/Debug/net6.0-windows/NEA Project.AssemblyInfo.cs
+++ NEA Project/obj/Debug/net6.0-windows/NEA Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NEA Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2cca436cdd019a44b31dd8836d61a59d2349424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NEA Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NEA Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
